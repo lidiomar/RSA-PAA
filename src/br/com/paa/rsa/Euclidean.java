18cce187f7c70981034af7627a925f54f7b7affe
Lidiomar Fernando Machado <lidiomarfernandomachado@Lidiomars-iMac.local>
2018-06-05 14:19:28 -0300

    comentario euclides

--- src/br/com/paa/rsa/Euclidean.java
+++ src/br/com/paa/rsa/Euclidean.java
@@ -4,15 +4,36 @@ import java.math.BigInteger;
 
 public class Euclidean {
 	
+	/*
+	 *  (b%a).x1 + a.y1 = gcd
+	 *  
+	 *   ax + by = gcd
+	 *   
+	 *   b%a = b - [b/a].a
+	 *   
+	 *   Substituindo:
+	 *   gcd = (b%a).x1 + a.y1 = ( b - [b/a].a ).x1 + ay1
+	 *   
+	 *   Agrupando os termos:
+	 *   
+	 *   gcd = b.x1 + a. (y1 - (b/a).x1 )
+	 *   
+	 *  Então:
+	 *  
+	 *  x = y1 - (b/a). x1
+	 *  y = x1
+	 *  
+	 * */
+	
 	public static BigInteger[] gcdExtended(BigInteger a, BigInteger b){
 		if (b.equals(BigInteger.ZERO))
-			return new BigInteger[] {a, BigInteger.ONE, BigInteger.ZERO};
+			return new BigInteger[] {a, BigInteger.ONE, BigInteger.ZERO}; // resposta GCD = a
 		                                                      
-		BigInteger[] triple = gcdExtended(b, a.mod(b)); 
+		BigInteger[] triple = gcdExtended(b, a.mod(b));
 		BigInteger x = triple[1];
-		triple[1] = triple[2];
-		triple[2] = x.subtract(a.divide(b).multiply(triple[2]));
-	
+		triple[1] = triple[2]; 
+		triple[2] = x.subtract(a.divide(b).multiply(triple[2])); //segundo multiplicador
+		
 		return triple;
 	}
 	public static BigInteger validateModularInverse(BigInteger b, BigInteger phi) {
@@ -28,4 +49,5 @@ public class Euclidean {
 		}
 		return gcd(q, p.mod(q));
 	}
+	
 }
